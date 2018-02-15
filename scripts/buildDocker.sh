ca07d8ffa9eb8464cf834343845cd77de206d46f
S. Mohammad M. Ziabary <softnhard.es@gmail.com>
2018-02-15 15:25:13 +0330

    docker builder updated in order to tag old version

--- scripts/buildDocker.sh
+++ scripts/buildDocker.sh
@@ -3,7 +3,7 @@
 pushd $(dirname $0) >/dev/null
 cd ..
 
-docker rmi --force targoman/amun_rest_server
+docker tag targoman/amun_rest_server:latest targoman/amun_rest_server:$(date +"%y%m%d_%H%M%S")
 docker build -t targoman/amun_rest_server -f scripts/docker/Dockerfile.server .
 
 popd > /dev/null
